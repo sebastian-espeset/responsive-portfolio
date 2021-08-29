12831a505b39e89638e93342236eadfcbb39a305
sebastian espeset <sebastian.c.espeset@gmail.com>
2021-08-28 20:55:46 -0500

    adds a bunch of stuff

---
+++ src/components/intro/Intro.jsx
@@ -0,0 +1,20 @@
+import "./intro.scss"
+
+export default function Intro() {
+    return (
+        <div className="intro" id="intro">
+            <div className="left">
+                <div className="imgContainer">
+                    <img src="assets/DSC_2743.jpg" alt="" />
+                </div>
+            </div>
+            <div className="right">
+                <div className="wrapper">
+                    <h2> Oh hi there</h2>
+                    <h2> I'm Sebastian Espeset</h2>
+                    <h3> Full stack web developer</h3>
+                </div>
+            </div>
+        </div>
+    )
+}
---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,33 @@
+import "./topbar.scss";
+import {NightsStay, Mail} from "@material-ui/icons";
+
+export default function Topbar({menuOpen,setMenuOpen}) {
+  
+    return (
+    <div className={"topbar "+ (menuOpen && "active")}>
+      <div className="wrapper">
+        <div className="left">
+          <a href="#intro" className="logo">
+            Smashing.
+          </a>
+          <div className="itemContainer">
+              <NightsStay className="icon"/>
+              <span>+ 44 12 4214 21</span>
+          </div>
+          <div className="itemContainer">
+              <Mail className="icon"/>
+            <span> seba@gmail.com</span>
+          </div>
+        </div>
+        <div className="right">
+            <div className="hamburger" onClick={()=>setMenuOpen(!menuOpen)}>
+                <span className="line1"></span>
+                <span className="line2"></span>
+                <span className="line3"></span>
+            </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
