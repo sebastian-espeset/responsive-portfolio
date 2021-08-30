19db6c59b7860a68f5fa825d8a6191fc64713955
sebastian espeset <sebastian.c.espeset@gmail.com>
2021-08-29 23:21:16 -0500

    builds out contact page

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,27 @@
 import "./contact.scss";
+import { useState } from "react";
 
 export default function Contact() {
+    const [message,setMessage]=useState(false);
+
+    const handleSubmit=(e)=>{
+        e.preventDefault();
+        setMessage(true)
+    }
     return (
         <div className="contact" id="contact">
-            
+            <div className="left">
+                <img src="assets/contact1.png" alt="" />
+            </div>
+            <div className="right">
+                <h2> Contact.</h2>
+                <form onSubmit={handleSubmit}>
+                    <input type="text" placeholder="email" />
+                    <textarea placeholder="message"></textarea>
+                    <button type="submit">Send</button>
+                    {message && <span>Thanks, I'll reply soon </span>}
+                </form>
+            </div>
         </div>
     )
 }
