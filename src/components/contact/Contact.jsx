bc0adcb4413e7a5e4e6322cd82803c285750c1a7
sebastian espeset <sebastian.c.espeset@gmail.com>
2021-09-16 13:35:52 -0500

    added email functionality, tweeks

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,28 +1,75 @@
+import React from "react";
 import "./contact.scss";
 import { useState } from "react";
+import emailjs from "emailjs-com";
+import dotenv from "dotenv";
 
-export default function Contact() {
-    const [message,setMessage]=useState(false);
+dotenv.config();
 
-    const handleSubmit=(e)=>{
-        e.preventDefault();
-        setMessage(true)
-    };
+//configure emailjs
 
-    return (
-        <div className="contact" id="contact">
-            <div className="left">
-                <img src="assets/contact1.png" alt="" />
-            </div>
-            <div className="right">
-                <h2> Contact.</h2>
-                <form onSubmit={handleSubmit}>
-                    <input type="text" placeholder="email" />
-                    <textarea placeholder="message"></textarea>
-                    <button type="submit">Send</button>
-                    {message && <span>Thanks, I'll reply soon </span>}
-                </form>
-            </div>
-        </div>
-    )
+const userId = process.env.REACT_APP_EMAILJSID
+
+
+const initialValues = {
+  from_name: "",
+  reply_to: "",
+  message: "",
 };
+export default function Contact() {
+
+  const [message, setMessage] = useState(false);
+  const [formData, setFormData] = useState(initialValues);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    
+    setMessage(true);
+
+    emailjs.send('from_portfolio_site','template_1m29p6p',formData,userId)
+      .then(res=>{
+        setFormData(initialValues)
+      }).catch(err=>{
+        console.log(err)
+      });
+  };
+
+  const changeHandler = (e) => {
+    e.preventDefault();
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  return (
+    <div className="contact" id="contact">
+      <div className="left">
+        <img src="assets/contact1.png" alt="" />
+      </div>
+      <div className="right">
+        <h2> Contact.</h2>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            name="from_name"
+            onChange={changeHandler}
+            placeholder="your name"
+          />
+          <input
+            type="email"
+            name="reply_to"
+            onChange={changeHandler}
+            placeholder="your@email.com"
+          />
+          <textarea
+            name="message"
+            type="text"
+            onChange={changeHandler}
+            placeholder="Say hi to me!"
+          ></textarea>
+          <button type="submit">Send</button>
+          {message && <span>Thanks, I'll reply soon </span>}
+        </form>
+      </div>
+    </div>
+  );
+}
